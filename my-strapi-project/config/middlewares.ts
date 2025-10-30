59791e2148c3ab62859ceef28036e6378c6a1849
David Akah <eliasakah1999@gmail.com>
2025-10-30 22:30:04 +0530

    modified the allow-control-access-origin to use the main vercel application

--- my-strapi-project/config/middlewares.ts
+++ my-strapi-project/config/middlewares.ts
@@ -10,7 +10,7 @@ export default [
           "connect-src": [
             "'self'",
             "https://funny-authority-3c58098ae3.strapiapp.com",
-            "https://savory-dish-hi42.vercel.app/",
+            "https://savory-dish-hi42-david-akahs-projects.vercel.app/",
           ],
         },
       },
@@ -19,7 +19,7 @@ export default [
   {
     name: "strapi::cors",
     config: {
-      origin: ["https://savory-dish-hi42.vercel.app/", "http://localhost:3000"], // ✅ only allow your frontend
+      origin: ["https://savory-dish-hi42-david-akahs-projects.vercel.app/", "http://localhost:3000"], // ✅ only allow your frontend
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       headers: ["Content-Type", "Authorization"],
       credentials: true,
